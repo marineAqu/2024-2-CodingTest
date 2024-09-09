f9fdf2d27659b6054d5e85a4d644dc0115bff9d1
marineAqu <102001809+marineAqu@users.noreply.github.com>
2024-09-09 13:41:20 +0900

    9월 9일 코테 연습

---
+++ src/main/java/org/example/Java0909.java
@@ -0,0 +1,74 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.SortedMap;
+
+public class Java0909 {
+    //2강 자료구조
+    Scanner scanner = new Scanner(System.in);
+
+    public void no3 (int n){
+        int sum = 0;
+        int firpoint = 1;
+        int laspoint = firpoint+1;
+        int answer = 1;
+
+        while (true){
+            if(firpoint == (n/2)+1) break;
+
+            //sum 구하기
+            sum = 0;
+            for(int i=firpoint; i<=laspoint; i++){
+                sum += i;
+            }
+
+            //점검
+            if(sum < n){
+                laspoint++;
+            }
+            else if(sum > n){
+                firpoint++;
+                laspoint = firpoint+1;
+            }
+            else{
+                //해당되는 경우 출력
+                System.out.println(n);
+                for(int i=firpoint; i<=laspoint; i++){
+                    System.out.print(i);
+                    if(i!=laspoint) System.out.print("+");
+                }
+                System.out.println();
+
+                firpoint++;
+                laspoint = firpoint + 1;
+                answer++;
+            }
+        }
+
+        System.out.println("총 개수: "+answer);
+    }
+
+    public void no10(){
+        //02. 자료구조 - 10번 슬라이드 문제
+        //백준 1253번
+        int goodNum = 0;
+        int firpoint, laspoint;
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+
+        for(int i=0; i<n; i++){
+            arr[i] = scanner.nextInt();
+        }
+
+        //정렬
+        Arrays.sort(arr);
+
+        for(int i=0; i<n; i++){
+            firpoint = 1;
+            laspoint = firpoint + 1;
+        }
+
+        System.out.println("좋은 수의 개수: "+goodNum);
+    }
+}
