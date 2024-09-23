52d7f06996e626b3bc8e9ff87fc555c9cfcfc1ee
marineAqu <102001809+marineAqu@users.noreply.github.com>
2024-09-23 14:41:44 +0900

    2장 자료구조 - 9월 23일

--- src/main/java/org/example/Java0909.java
+++ src/main/java/org/example/Java0909.java
@@ -1,7 +1,10 @@
 package org.example;
 
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
 
 public class Java0909 {
     //2강 자료구조
@@ -96,5 +99,67 @@ public class Java0909 {
         }
 
         System.out.println("좋은 수의 개수: "+goodNum);
+    }
+
+    //0923 슬라이딩 윈도우 & 스택과 큐
+
+    static class Node{
+        int value;
+        int index;
+
+        public Node(int value, int index){
+            this.value = value;
+            this.index = index;
+        }
+    }
+
+    public void no21() throws IOException {
+        //21번 슬라이드 문제, 백준 11003
+
+        //12개 파일
+        /*
+        BufferedReader br = new BufferedReader(new FileReader("C:\\Codingtest2024\\src\\main\\resources\\SlidingWindow_12.txt"));
+        int L = scanner.nextInt();
+        int n = 12;
+         */
+
+        //100개 파일
+        BufferedReader br = new BufferedReader(new FileReader("C:\\Codingtest2024\\src\\main\\resources\\SlidingWindow_100.txt"));
+        int L = scanner.nextInt();
+        int n = 100;
+
+
+        String temp = br.readLine();
+
+        Deque<Node> deque = new ArrayDeque<>();
+
+        for(int i=0; i<n; i++) {
+            int now = Integer.parseInt(temp.split(" ")[i]);
+
+            while (!deque.isEmpty() && deque.getLast().value > now) {
+                deque.removeLast();
+            }
+            deque.addLast(new Node(now, i));
+
+            if(deque.getFirst().index <= i-L) deque.removeFirst();
+            System.out.print(deque.getFirst().value+" ");
+        }
+    }
+
+    public void no36(){
+        //36번 슬라이드 문제, 백준 2164
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        int n = scanner.nextInt();
+
+        for(int i=1; i<=n; i++) queue.add(i);
+
+        while (queue.size() > 1){
+            queue.poll();
+            queue.add(queue.poll());
+        }
+
+        System.out.println(queue.poll());
     }
 }
